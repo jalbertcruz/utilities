21b160fcb3865d6a67f14d9187d984d9a998cc83
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2015-04-25 18:38:15 -0400

    Subfolder control & authored_date analysis added
    Signed-off-by: José Albert Cruz Almaguer <jalbertcruz@gmail.com>

--- git/git-flows/GitFlowUtilities/repo.py
+++ git/git-flows/GitFlowUtilities/repo.py
@@ -11,24 +11,23 @@
 import git
 import os
 import subprocess
+import json
+import time
 
 
 class RepoCommitsTool:
     def __init__(self, path="."):
+        self.path = path
         self.r = git.Repo(path)
 
     def commits_in(self, path):
-        # prefix = 'commit '
-        # return [l[len(prefix):] for l in self.r.git.log(path).split("\n") if l.startswith(prefix)]
         return self.r.iter_commits(paths=path)
 
     def commits(self):
-        # return self.commits_in(".")
         return self.r.iter_commits()
 
-    @staticmethod
-    def dirs():
-        return [f for f in os.listdir(".") if os.path.isdir(f) and not f.startswith('.')]
+    def dirs(self):
+        return json.loads("".join(open(os.path.join(self.path, 'config/mirrors.json')).readlines())).keys()
 
     def is_consistent(self):
         dirs_commits = [set([c.hexsha for c in self.commits_in(a)]) for a in self.dirs()]
@@ -42,49 +41,94 @@ class RepoCommitsTool:
     def active_branch(self):
         return self.r.active_branch
 
-    def number_of_commits(self):
-        return len(list(self.commits()))
-
-    def number_of_commits_in(self, path):
-        return len(list(self.commits_in(path)))
-
     def check_commits_in_dir(self):
         if not self.is_consistent():
             print("The commits must be over only one project")
             cmd = "git reset --mixed HEAD^"
             subprocess.call(cmd.split(' '))
 
+    def check_sync(self, dest, k):
+        origin_commits = list(self.commits_in(k))
+        ncom_origin = len(origin_commits)
+        dest_commits = list(dest.commits_in(k))
+        ncom_dest = len(dest_commits)
+
+        origin_commits.sort(key=lambda x: x.authored_date, reverse=True)
+        dest_commits.sort(key=lambda x: x.authored_date, reverse=True)
+
+        if self.active_branch() != dest.active_branch():
+            res = (1, None)
+        else:
+            if ncom_origin == ncom_dest:
+                res = (2, None)
+            else:
+                if ncom_origin < ncom_dest:
+                    res = (3, None)
+                else:
+                    res = (4, [ncom_origin, ncom_dest])
+        if res[0] != 1:
+            n = min(ncom_dest, ncom_origin)
+            origin_commits = origin_commits[ncom_origin - n:]
+            dest_commits = dest_commits[ncom_dest - n:]
+            i = n - 1
+            flag = True
+            while i > -1 and flag:
+                # if origin_commits[i].committed_date != dest_commits[i].committed_date:
+                if origin_commits[i].authored_date != dest_commits[i].authored_date:
+                    flag = False
+                    res = (5, origin_commits[i], dest_commits[i])
+                else:
+                    i -= 1
+        return res
+
 
 class SingleGitRepoFlow:
     @staticmethod
     def gsync(config):
         """ Utlidad orientada a automatizar la sincronizacion de multiples repos git locales-privados
-        con un repo git central-publico.
-        Ambos repos se sincronizan a partir del commit 2 (tiene que existir un commit inicial)
+            con un repo git central-publico. (tiene que existir al menos un commit en cada repo)
         """
         for k in config.keys():
             origin = RepoCommitsTool()
             dest = RepoCommitsTool(config[k])
-            ncom_origin = origin.number_of_commits_in(k)
-            ncom_dest = dest.number_of_commits_in(k)
 
-            if origin.active_branch() != dest.active_branch():
-                print("Diferent active_branch in: " + k)
-            else:
-                if ncom_origin == ncom_dest:
-                    print("Already in sync in: " + k)
-                else:
-                    if ncom_origin < ncom_dest:
-                        print("Origin outdated in: " + k)
-                    else:
-                        diff = ncom_origin - ncom_dest
-                        commits = list(origin.commits_in(k))[:diff]
-                        num = 1
-                        commits.reverse()
-                        print("Creating patchs for: " + k)
-                        for com in commits:
-                            subprocess.call(
-                                ["git", "format-patch", "-1", com.hexsha, "-o", k + "-new-commits", "-s",
-                                 "--start-number",
-                                 str(num)])
-                            num += 1
+            res = origin.check_sync(dest, k)
+
+            def st_same_branch():
+                print("Different active_branch in: " + k)
+
+            def st_in_sync():
+                print("Already in sync in: " + k)
+
+            def st_origin_outdated():
+                print("Origin outdated in: " + k)
+
+            def st_ok():
+                diff = res[1][0] - res[1][1]
+                commits = list(origin.commits_in(k))[:diff]
+                num = 1
+                commits.reverse()
+                print()
+                print("Creating patchs from: " + k)
+                for com in commits:
+                    subprocess.call(
+                        ["git", "format-patch", "-1", com.hexsha, "-o", "new-commits-" + k.replace('/', '-'),
+                         "-s", "--start-number",
+                         str(num)])
+                    num += 1
+
+            def st_different_history():
+                print("Different history in: " + k)
+                print("First different datetime in origin at", res[1].hexsha)
+                print(time.ctime(res[1].authored_date), "/", time.ctime(res[2].authored_date))
+                print("Origin:", res[1].message, "vs", "\nDest:", res[2].message)
+
+            cases = {
+                1: st_same_branch,
+                2: st_in_sync,
+                3: st_origin_outdated,
+                4: st_ok,
+                5: st_different_history
+            }
+
+            cases[res[0]]()
